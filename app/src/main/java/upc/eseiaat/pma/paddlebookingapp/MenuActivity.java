3cb1fc07a6f6e3bd21ffb6f4e259b60d68bd2a7e
eugerodr <eugeniarodriguez.eis@gmail.com>
2017-12-04 18:12:48 +0100

    Últimos retoques layout

--- app/src/main/java/upc/eseiaat/pma/paddlebookingapp/MenuActivity.java
+++ app/src/main/java/upc/eseiaat/pma/paddlebookingapp/MenuActivity.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MenuActivity extends AppCompatActivity {
 
