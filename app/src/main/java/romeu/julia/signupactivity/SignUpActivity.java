f65a13ab2337b6518560b17875f3891cb7468c89
juliaromeu <juliaromeu03@gmail.com>
2017-12-11 16:49:51 +0100

    Objects in Layout Experience array in the Spinner

--- app/src/main/java/romeu/julia/signupactivity/SignUpActivity.java
+++ app/src/main/java/romeu/julia/signupactivity/SignUpActivity.java
@@ -2,6 +2,9 @@ package romeu.julia.signupactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Spinner spinner_experience = (Spinner) findViewById(R.id.spinner_experience);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.experience_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_experience.setAdapter(adapter);
+
+
+
     }
 }
