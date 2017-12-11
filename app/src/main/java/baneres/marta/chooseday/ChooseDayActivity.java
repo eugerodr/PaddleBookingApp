d20d34c813cbe4a6c1e0de2c345eec8c1932a155
martabaneres <martabaneres@hotmail.com>
2017-12-11 16:15:36 +0100

    Hay un EditText en el que pone "Select Date...", al clicar en el aparece un calendario para seleccionar la fecha. Al dar "OK" a la fecha, vuelve al EditText en el que se habrá escrito la fecha seleccionada.

---
+++ app/src/main/java/baneres/marta/chooseday/ChooseDayActivity.java
@@ -0,0 +1,46 @@
+package baneres.marta.chooseday;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class ChooseDayActivity extends AppCompatActivity {
+
+    EditText date;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_day);
+        date = (EditText) findViewById(R.id.date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(ChooseDayActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+    }
+}
