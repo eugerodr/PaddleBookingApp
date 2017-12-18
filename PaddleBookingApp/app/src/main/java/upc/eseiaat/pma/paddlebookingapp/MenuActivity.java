2d37337012e4d4fe9341cdc1aaed284a9995d386
eugerodr <eugeniarodriguez.eis@gmail.com>
2017-12-18 15:45:56 +0100

    Al hacer click en una reserva, te lleva a la actividad ViewReservationActivity, donde puedes ver y/o cancelar la reserva

--- PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/MenuActivity.java
+++ PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/MenuActivity.java
@@ -1,11 +1,15 @@
 package upc.eseiaat.pma.paddlebookingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,6 +19,10 @@ public class MenuActivity extends AppCompatActivity {
     private ArrayList<String> lista_reservas;
     private ArrayAdapter adapter;
     private ListView lista;
+    private String data;
+    private String player_1_data="Julia";
+    private String player_2_data="Marta";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,26 @@ public class MenuActivity extends AppCompatActivity {
         adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, lista_reservas);
         lista.setAdapter(adapter);
 
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //cojo los datos en la posicion del item
+                data = lista_reservas.get(position);
+                // (I)
+                // 1. Crear un 'intent'
+                Intent intent = new Intent(getApplicationContext(), ViewReservationActivity.class);
+                // 2. Afegir paràmetres (dades extra) a la crida a l'activitat
+                intent.putExtra("data",  data);
+                intent.putExtra("player_1_data", player_1_data);
+                intent.putExtra("player_2_data", player_2_data);
+                // 3. Passar l'intent a Android perquè obri l'activitat
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,4 +67,16 @@ public class MenuActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 0:
+                // (IV)
+                if (resultCode == AppCompatActivity.RESULT_OK) {
+                     data = data.getStringExtra("data");
+                    data.setText(data);
+                }
+        }
+    }*/
 }
--- PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/ViewReservationActivity.java
+++ PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/ViewReservationActivity.java
@@ -1,8 +1,50 @@
 package upc.eseiaat.pma.paddlebookingapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 /**
  * Created by Eugenia on 11/12/2017.
  */
 
-public class ViewReservationActivity {
+public class ViewReservationActivity extends AppCompatActivity {
+
+    //private String title = "Día y hora de la reserva...";
+    private TextView date_reservation;
+    private TextView player_1;
+    private TextView player_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_reservation);
+
+        // (II)
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        String player_1_data = intent.getStringExtra("player_1_data");
+        String player_2_data = intent.getStringExtra("player_2_data");
+        date_reservation = (TextView) findViewById(R.id.date_reservation);
+        date_reservation.setText(data);
+        player_1 = (TextView) findViewById(R.id.player_1);
+        player_1.setText(player_1_data);
+        player_2 = (TextView) findViewById(R.id.player_2);
+        player_2.setText(player_2_data);
+
+    }
+
+    /*public void saveDate(View view) {
+        // (III)
+        String new_data = date_reservation.getText().toString();
+        Intent data = new Intent();
+        data.putExtra("data", new_data);
+        finish();
+    }*/
+
 }
