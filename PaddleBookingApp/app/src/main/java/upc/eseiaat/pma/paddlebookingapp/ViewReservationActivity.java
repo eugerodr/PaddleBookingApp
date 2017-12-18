3b80a4529376237a9510a9f088797cda7510df50
eugerodr <eugeniarodriguez.eis@gmail.com>
2017-12-18 16:49:38 +0100

    Creación del método cancel_reservation, que muestra un Alert Dialog por pantalla, pero aún no elimina el elemento de la lista

--- PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/ViewReservationActivity.java
+++ PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/ViewReservationActivity.java
@@ -1,11 +1,16 @@
 package upc.eseiaat.pma.paddlebookingapp;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -37,7 +42,34 @@ public class ViewReservationActivity extends AppCompatActivity {
         player_2 = (TextView) findViewById(R.id.player_2);
         player_2.setText(player_2_data);
 
+        Button cancel_reserv = (Button) findViewById(R.id.cancel);
+        cancel_reserv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancel_reservation();
+            }
+        });
+
     }
+
+    private void cancel_reservation(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ViewReservationActivity.this);
+        builder.setTitle(R.string.confirm);
+        builder.setMessage(R.string.message);
+        builder.setPositiveButton(R.string.erase, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ViewReservationActivity.this, R.string.cancelled,
+                        Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        builder.setNegativeButton(R.string.cancel, null);
+
+        builder.create().show();
+            }
+
 
     /*public void saveDate(View view) {
         // (III)
