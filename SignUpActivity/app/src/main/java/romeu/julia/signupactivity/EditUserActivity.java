c30a36fc6a021e67f76f05d4033a6505ef82be8c
juliaromeu <juliaromeu03@gmail.com>
2018-01-05 16:44:06 +0100

    Obtener referencias del layout y información del usuario

--- SignUpActivity/app/src/main/java/romeu/julia/signupactivity/EditUserActivity.java
+++ SignUpActivity/app/src/main/java/romeu/julia/signupactivity/EditUserActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,34 @@ public class EditUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
 
+        //Referencias de los elementos del Layout
+        EditText edit_name = (EditText) findViewById(R.id.edit_name);
+        EditText edit_age = (EditText) findViewById(R.id.edit_age);
 
+        //Configuración Spinner
+        final Spinner spinner_experience = (Spinner) findViewById(R.id.spinner_experience);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.experience_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_experience.setAdapter(adapter);
+
+        //Información del usuario
+        String user_name = edit_name.getText().toString();
+        Integer user_age = Integer.parseInt(edit_age.getText().toString());
+        spinner_experience.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View view, int pos, long id) {
+                Integer user_level = spinner_experience.getSelectedItemPosition();
+                //Toast.makeText(SignUpActivity.this,"Selected:" + pos, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Do nothing
+            }
+        }
     }
 }
--- SignUpActivity/app/src/main/java/romeu/julia/signupactivity/ViewUserActivity.java
+++ SignUpActivity/app/src/main/java/romeu/julia/signupactivity/ViewUserActivity.java
@@ -1,7 +1,9 @@
 package romeu.julia.signupactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewUserActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class ViewUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_user);
 
-        //TODO:Obtener referencias de los elementos del Layout
+        String user_name = "James";
+        int user_age = 0;
+        int user_level = 0;
+
+        //Referencias de los elementos del Layout
+        TextView txt_name_surname = (TextView) findViewById(R.id.txt_name_surname);
+        TextView txt_age = (TextView) findViewById(R.id.txt_age);
+        TextView txt_level = (TextView) findViewById(R.id.txt_level);
+        
     }
 }
