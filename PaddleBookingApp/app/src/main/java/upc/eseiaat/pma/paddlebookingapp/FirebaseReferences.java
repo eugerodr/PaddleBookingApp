08df574954b9f09bc2201e0277d65358d2e356f6
juliaromeu <juliaromeu03@gmail.com>
2018-01-14 11:47:27 +0100

    Añadir FirebaseReference como clase java

---
+++ PaddleBookingApp/app/src/main/java/upc/eseiaat/pma/paddlebookingapp/FirebaseReferences.java
@@ -0,0 +1,12 @@
+package upc.eseiaat.pma.paddlebookingapp;
+
+/**
+ * Created by juliaromeu on 13/1/18.
+ */
+
+public class FirebaseReferences {
+
+    final public static String usersReference = "users";
+    final public static String userReference = "user";
+
+}
